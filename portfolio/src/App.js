21d9b88d58e45e5fa28bda0f40520807e500d1a2
jm macapagal <jmdmacapagal@gmail.com>
2019-04-22 19:28:43 +0800

    added Showcase component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
+import Showcase from './components/Showcase'
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Showcase />
     </div>
   )
 }
---
+++ portfolio/src/components/Showcase.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Showcase() {
+    return (
+        <section id="showcase">
+            <div class="container">
+                <h1>@ jmdmacapagal</h1>
+                <h3>&lt; Web Developer | <span id="javascript">JavaScript</span> <span id="nodejs">NodeJS</span> <span id="react">React</span> &gt;
+                </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nam voluptatem porro reiciendis neque vero fugiat voluptas. Sunt aperiam fuga, laudantium dolor est, eaque deserunt ut temporib!</p>
+                <a href="#portfolio"><button>Check Me Out!</button></a>
+            </div>
+        </section>
+    )
+}
+
+export default Showcase
