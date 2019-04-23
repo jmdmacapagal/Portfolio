af112461766b682417533ef8f537604277cfab57
jm macapagal <jmdmacapagal@gmail.com>
2019-04-24 06:01:07 +0800

    Updates

---
+++ precache-manifest.3226f616cb916c15e1457cbefe7fb0b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "12615edc0be99844b5db011fc759cfba",
+    "url": "/public/src/index.html/static/media/todo.12615edc.PNG"
+  },
+  {
+    "revision": "ce9cbab62b856f4bd44a083438551ffa",
+    "url": "/public/src/index.html/static/media/tempConverter.ce9cbab6.PNG"
+  },
+  {
+    "revision": "fbe3f54e3bcfe0d78efa4d2d9938bd8d",
+    "url": "/public/src/index.html/static/media/janken.fbe3f54e.PNG"
+  },
+  {
+    "revision": "d5e2fff268b11644ca8fddb730137fa4",
+    "url": "/public/src/index.html/static/media/engagementSite.d5e2fff2.PNG"
+  },
+  {
+    "revision": "dcd1965e4953e011cfc7",
+    "url": "/public/src/index.html/static/js/runtime~main.f6923d62.js"
+  },
+  {
+    "revision": "73d9c6dea13d67805a28",
+    "url": "/public/src/index.html/static/js/main.8fab0bd5.chunk.js"
+  },
+  {
+    "revision": "2a030385037c72458674",
+    "url": "/public/src/index.html/static/js/2.07af2cd4.chunk.js"
+  },
+  {
+    "revision": "73d9c6dea13d67805a28",
+    "url": "/public/src/index.html/static/css/main.689ebc91.chunk.css"
+  },
+  {
+    "revision": "312e6ebf4e4e060f2e387a40b18e6474",
+    "url": "/public/src/index.html/index.html"
+  }
+];
